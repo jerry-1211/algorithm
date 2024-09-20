cb509809e34e2f68c3715d7643854d677362e9b4
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2024-09-20 21:31:17 +0900

    [Silver I] Title: 트리 순회, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,42 +1,39 @@
-
 class Node():
     def __init__(self,item,left,right):
         self.item = item
         self.left = left
         self.right = right
 
-tree = {}
-# tree["A"] = Node("A","B","C")
+def preorder(node):
+    print(node.item,end='')
+    if node.left != ".":
+        preorder(tree[node.left])    
+    if node.right != ".":
+        preorder(tree[node.right])    
+    
+def inorder(node):
+    if node.left != ".":
+        inorder(tree[node.left])    
+    print(node.item,end='')
+    if node.right != ".":
+        inorder(tree[node.right])    
 
+def postorder(node):
+    if node.left != ".":
+        postorder(tree[node.left])    
+    if node.right != ".":
+        postorder(tree[node.right])    
+    print(node.item,end='')
+
+tree = {}
 N = int(input())
+
 for _ in range(N):
     item,left,right = map(str,input().split())
     tree[item] = Node(item,left,right)
 
-
-def preorder(node):
-    print(node.item,end="")
-    if node.left != ".":
-        preorder(tree[node.left])
-    if node.right != ".":
-        preorder(tree[node.right])
-
-def midorder(node):
-    if node.left != ".":
-        midorder(tree[node.left])
-    print(node.item,end="")    
-    if node.right != ".":
-        midorder(tree[node.right])
-
-def lastorder(node):
-    if node.left != ".":
-        lastorder(tree[node.left])
-    if node.right != ".":
-        lastorder(tree[node.right])
-    print(node.item,end="")    
-
 preorder(tree["A"])
 print()
-midorder(tree["A"])
+inorder(tree["A"])
 print()
-lastorder(tree["A"])
+postorder(tree["A"])
