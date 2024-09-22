db7983c7f1a950fe5bceae6c3880dd412c2cf9d3
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2024-09-22 20:55:38 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2344 ms, Memory: 424776 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,38 +1,33 @@
+
 import sys
 input = sys.stdin.readline
-#recursion error 방지
 sys.setrecursionlimit(10**9)
 
-arr = []
-while True:
-    try:
-        x = int(input())
-        arr.append(x)
+lst = []
+while True :
+    try: 
+        num = int(input())
+        lst.append(num)
     except:
         break
 
-
-def solution(A):
-    # 받은 배열 길이가 0이면 return
-    if len(A) == 0:
-        return
-
-    tempL, tempR = [], []
-    # 첫번째 값을 루트 노드로 설정
-    mid = A[0]
-    # 나머지 배열에 대해 for문을 돌면서 루트보다 커지는 부분을 기록해서 L과 R로 나눈다.
-    for i in range(1, len(A)):
-        if A[i] > mid:
-            tempL = A[1:i]
-            tempR = A[i:]
+def solution(tmp):
+    if len(tmp) == 0 :
+        return 
+    
+    tmpL,tmpR = [],[]
+    mid = tmp[0]
+    for i in range(1,len(tmp)):
+        if tmp[i] > mid:
+            tmpL = tmp[1:i]
+            tmpR = tmp[i:]
             break
     else:
-    	#모두 mid보다 작은 경우
-        tempL = A[1:]
+        tmpL = tmp[1:]
     
-    #왼쪽, 오른쪽 순으로 재귀 후 루트 노드 값 출력
-    solution(tempL)
-    solution(tempR)
+    solution(tmpL)
+    solution(tmpR)
     print(mid)
 
-solution(arr)
+
+solution(lst)
