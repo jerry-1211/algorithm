470ba6f9f8586d40c13930d10f11c88c0509fe09
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2024-09-26 18:01:27 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1704 ms, Memory: 99944 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
+
 N,M,K,X = map(int,input().split())
 linked_lst = [[] for _ in range(N+1)]
 cost = [-1] * (N+1)
@@ -13,17 +13,22 @@ for _ in range(M):
 q =deque([X])
 cost[X] = 0
 
+
 while q:
     v = q.popleft()
-
+    
     for e in linked_lst[v]:    
         if cost[e] == -1 :
             cost[e] = cost[v] + 1
             q.append(e)
 
-if K not in cost:
+found = False
+
+
+for idx,i in enumerate(cost):
+    if i == K :         
+        print(idx)
+        found = True
+    
+if not found:
     print(-1)
-else:  
-    for i in range(len(cost)):
-        if cost[i] == K :
-            print(i)
