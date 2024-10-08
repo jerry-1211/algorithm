e8204a0903137d38bc4a4ec37a5a5cd2ed6bb884
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2024-10-08 09:42:37 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1132 ms, Memory: 70184 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+
+INF = sys.maxsize
+dp = [INF]*(N*4)
+dp[0],dp[1] = 0,0
+
+for i in range(1,N+1):
+    dp[i*3] = min(dp[i]+1, dp[i*3])
+    dp[i*2] = min(dp[i]+1, dp[i*2])
+    dp[i+1] = min(dp[i]+1, dp[i+1])
+
+
+dp = dp[:N+1]
+cur = N
+lst = [N]
+
+
+for k in range(N,0,-1):
+    if dp[k] == dp[cur]-1 and ((k+1)==cur or k*3==cur or (k*2)==cur) :
+        lst.append(k)
+        cur = k
+
+print(dp[N])
+print(*lst)
