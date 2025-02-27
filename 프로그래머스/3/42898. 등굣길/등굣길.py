faf8a73fe47de12374a1fa589071f7649397170e
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-02-27 11:54:15 +0900

    [level 3] Title: 등굣길, Time: 1.86 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -2,23 +2,27 @@ def solution(m, n, puddles):
     dp = [[0] * m for _ in range(n)]
     
     # puddles 설정
-    puddles = [[q,p] for [p,q] in puddles]
-    for i,j in puddles :
+    for j,i in puddles :
         dp[i-1][j-1] = -1
+    
+
     
     dp[0][0] = 1
     
-    for i in range(1,n):
-        if dp[i][0] == -1 :
-            dp[i][0] = 0
-        else: 
-            dp[i][0] = dp[i-1][0]
-                
-    for j in range(1,m):
-        if dp[0][j] == -1 :
-            dp[0][j] = 0
+    # 첫 번째 열 초기화 (웅덩이 이후는 0)
+    for i in range(1, n):
+        if dp[i][0] == -1:
+            dp[i][0] = 0  # 웅덩이면 이후는 전부 0
         else:
-            dp[0][j] = dp[0][j-1] 
+            dp[i][0] = dp[i-1][0]
+    
+    # 첫 번째 행 초기화 (웅덩이 이후는 0)
+    for j in range(1, m):
+        if dp[0][j] == -1:
+            dp[0][j] = 0  # 웅덩이면 이후는 전부 0
+        else:
+            dp[0][j] = dp[0][j-1]
+    
         
     for i in range(1,n):
         for j in range(1,m):
