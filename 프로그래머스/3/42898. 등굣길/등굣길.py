b3f5ef2c95c29363f35003ed41a6d12b36792f2d
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-02-28 11:42:11 +0900

    [level 3] Title: 등굣길, Time: 4.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,38 +1,22 @@
 def solution(m, n, puddles):
-    dp = [[0] * m for _ in range(n)]
-    
-    # puddles 설정
-    for j,i in puddles :
-        dp[i-1][j-1] = -1
-    
+    answer = 0 
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    puddles = [[q,p] for p,q in puddles]
 
-    
-    dp[0][0] = 1
-    
-    for i in range(1, n):
-        if dp[i][0] == -1:
-            dp[i][0] = 0  
-        else:
-            dp[i][0] = dp[i-1][0]
-    
-    for j in range(1, m):
-        if dp[0][j] == -1:
-            dp[0][j] = 0
-        else:
-            dp[0][j] = dp[0][j-1]
-    
-        
-    for i in range(1,n):
-        for j in range(1,m):
+    for i in range(1,n+1):
+        for j in range(1,m+1):
             
-            if dp[i][j] == -1 : 
-                dp[i][j] = 0
-                continue 
+            if i == j == 1:
+                dp[1][1] = 1
+                continue
                 
-            
-            dp[i][j] = (dp[i][j-1]  + dp[i-1][j])% 1000000007
+            if [i,j] in puddles : 
+                continue
                 
-        
-    answer = dp[-1][-1]
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) %1000000007
     
-    return answer
+    
+            
+            
+    
+    return dp[-1][-1]
