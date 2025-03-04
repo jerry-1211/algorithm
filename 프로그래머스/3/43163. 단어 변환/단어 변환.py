b75fd7299e0a9826f50475fdf6d81aa4ea77b61d
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-03-04 11:24:55 +0900

    [level 3] Title: 단어 변환, Time: 0.57 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,29 +1,28 @@
 from collections import deque 
 def solution(begin, target, words):
     answer = 0
+    NOT_VISIT = 0 
     
     if target not in words:
         return 0 
     
-    words = [[0,words[i]] for i in range(len(words))]
-    
-    q = deque([[0,begin]])
-    
+    visit_words = [[NOT_VISIT,words[i]] for i in range(len(words))]
+    q = deque([[NOT_VISIT,begin]])
     
     while q:
-        v = q.popleft()
-        if v[1] == target:
-            return v[0]
+        current_depth, current_word = q.popleft()
+        if current_word == target:
+            return current_depth
         
-        for k in range(len(words)):
-            if words[k][0] == 0:  # 방문하지 않는 경우 
+        for k in range(len(visit_words)):
+            if visit_words[k][0] == 0:  # 방문하지 않는 경우 
                 count = 0 
-                for i in range(len(words[k][1])):
-                    if words[k][1][i] != v[1][i]: # 방문하지 않은 words와 현재 v 비교 
+                for i in range(len(visit_words[k][1])):
+                    if visit_words[k][1][i] != current_word[i]: # 방문하지 않은 words와 현재 v 비교 
                         count += 1
                                 
                 if count == 1 :
-                    q.append([v[0]+1,words[k][1]])
-                    words[k][0] = 1
+                    q.append([current_depth+1,visit_words[k][1]])
+                    visit_words[k][0] = 1 
     
     return answer
