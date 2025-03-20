263d97f3e1ecb524d396c8ac95009f473beddbf9
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-03-20 15:15:02 +0900

    [Silver III] Title: iSharp, Time: 152 ms, Memory: 16808 KB -BaekjoonHub

--- 백준/Silver/3568. iSharp/iSharp.java
+++ 백준/Silver/3568. iSharp/iSharp.java
@@ -1,35 +1,33 @@
 import java.io.*;
 
+
 public class Main {
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
-
-        String in =br.readLine();
-        String [] data = in.split(" ");
-
-        for (int i = 1; i < data.length; i++) {
-            getParam(data[0], data[i]);
+        String [] arr = br.readLine().split(" ");
+        for (int i = 1; i < arr.length; i++) {
+            getParam(arr[0], arr[i]);
         }
         System.out.println(sb.toString());
     }
 
     public static void  getParam(String type, String str){
         String out = "";
-        StringBuilder sym = new StringBuilder();
+        StringBuilder tmpSb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char cur = str.charAt(i);
-            if(cur=='*' || cur=='&'){
-                sym.append(cur);
-            }else if(cur =='['){
-                sym.append(']');
-            }else if(cur == ']'){
-                sym.append('[');
-            }else if (('A' <= cur && cur <='Z') || ('a' <= cur && cur <='z')){
-                out += str.charAt(i);
+            if (cur == '*' || cur == '&'){
+                tmpSb.append(cur);
+            }else if(cur == '['){
+                tmpSb.append(']');
+            } else if (cur == ']') {
+                tmpSb.append('[');
+            }else if (('a'<= cur && cur<='z') ||('A'<= cur && cur<='Z') ){
+                out += cur;
             }
         }
-        sb.append(type+sym.reverse()+" " + out+";\n");
-
+        sb.append(type + tmpSb.reverse() + " " + out + ";\n");
     }
+
 }
