b2acb6ae34ec11aaeddec7c0169c640cda58a6d7
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-06 11:10:46 +0900

    [Silver II] Title: 창고 다각형, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -11,14 +11,14 @@ for i in range(len(arr)):
 
 result = 0
 left = arr[0]
-for i in range(1,mx_idx+1):
+for i in range(mx_idx+1):
     if arr[i][1] >= left[1]:
         result += left[1] * (arr[i][0] - left[0])
         left = arr[i]
 
 
 right = arr[-1]
-for i in range(len(arr)-2, mx_idx-1, -1):
+for i in range(len(arr)-1, mx_idx-1, -1):
     if (arr[i][1] >= right[1]):
         result += right[1] * (- arr[i][0] + right[0])
         right = arr[i]
