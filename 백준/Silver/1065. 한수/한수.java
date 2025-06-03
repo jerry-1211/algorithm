f5763cd0dcb8140cac6255e464fb1beb9216be6c
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-03 10:44:54 +0900

    [Silver IV] Title: 한수, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static ArrayList<String> result;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+
+
+        if (N >= 100 ){
+            for (int i = 100; i < N+1; i++) {
+                int hundreds = i/100;
+                int tens = (i%100) / 10;
+                int ones = i % 10;
+
+                if(tens-ones == hundreds-tens){
+                    count++;
+                }
+            }
+            count += 99;
+        }else{
+            count = N;
+        }
+
+
+        System.out.println(count);
+
+    }
+}
+
+
