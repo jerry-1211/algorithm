1890245a566f61d92dbbd78f7c0a3ce7f78c1d9f
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-06 16:45:55 +0900

    [Gold V] Title: 톱니바퀴, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,53 +1,58 @@
-import sys
-from collections import deque
+wheel = [[] for _ in range(4)]
 
-input = sys.stdin.readline
+def turn_wheel(arr,dir):
+    if dir == 1:
+        return [arr[-1]] + arr[:-1]  # 시계방향: 마지막 원소를 맨 앞으로
+    else:
+        return arr[1:] + [arr[0]]    # 반시계방향: 첫 원소를 맨 뒤로
 
-# 방향 로테이션
-def wheel_rotate(graph,i,dir):
-    graph[i].rotate(dir)
-    return 
+def check(before_wheel, after_wheel):
+    if before_wheel[2] != after_wheel[6]:
+        return True
+    return False
 
+for i in range(4):
+    wheel[i] = (list(map(int,list(input()))))
 
-# 방향 로테이션 스택에 넣어주기
-def stack_rotate(graph,n,dir):
-    stack = [(n,dir)]
+T = int(input())
+
+for _ in range(T):
+    idx,dir = map(int,input().split(" "))
+    idx -= 1
+
+    # 톱니바퀴 사전체크 (회전 전 상태로 체크)
+    check_lst = [False,False,False]
+    for i in range(3):
+        if check(wheel[i],wheel[i+1]):
+            check_lst[i] = True
+
+    # 각 톱니바퀴의 회전 방향 결정
+    check_way = [0,0,0,0]
+    check_way[idx] = dir
     
-    rdir = -dir
-    for i in range(n,0,-1):
-        if graph[i][6] != graph[i-1][2]:
-            stack.append([i-1,rdir])
-            rdir = -rdir
-            continue
-        break
-
-    rdir = -dir
-    for i in range(n,3):
-        if graph[i][2] != graph[i+1][6]:
-            stack.append([i+1,rdir])
-            rdir = -rdir
-            continue
-        break
-
-    for i,dir in stack:
-        wheel_rotate(graph,i,dir)
-
-    return 
-
-def main():
-    graph = [deque(list(map(int,list(input().strip())))) for _ in range(4)]
-    K = int(input())
-
-
-    for _ in range(K):
-        n,dir = map(int,input().split())  
-        stack_rotate(graph,n-1,dir)
-
-
-    rs = 0 
-    for k in range(4):
-        rs += graph[k][0] * (2**k)
-    print(rs)
+    # 오른쪽으로 전파
+    for i in range(idx, 3):
+        if check_lst[i]:
+            check_way[i+1] = -check_way[i]  # 반대 방향
+        else:
+            break
     
-if __name__ == "__main__":
-    main()
+    # 왼쪽으로 전파
+    for i in range(idx-1, -1, -1):
+        if check_lst[i]:
+            check_way[i] = -check_way[i+1]  # 반대 방향
+        else:
+            break
+
+    # 실제 회전 수행
+    for i in range(4):
+        if check_way[i] != 0:
+            wheel[i] = turn_wheel(wheel[i], check_way[i])
+
+# 점수 계산
+score = 0
+for i in range(4):
+    if wheel[i][0] == 1:  # 12시 방향이 S극(1)이면
+        score += 2 ** i
+
+print(score)
