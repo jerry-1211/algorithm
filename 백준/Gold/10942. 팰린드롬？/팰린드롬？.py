10140b7cec3a00160426490275298e6861f0afaf
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-09 18:18:43 +0900

    [Gold IV] Title: 팰린드롬?, Time: 644 ms, Memory: 153916 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -2,26 +2,50 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = list(map(int, input().split()))
-dp = [[0] * N for _ in range(N)]
+arr = list(map(int,input().split()))
+dp = [[0]*N for i in range(N)]
 
-# 길이 1인 팰린드롬 (모든 단일 수는 팰린드롬)
-for i in range(N):
+
+for i in range(len(arr)):
     dp[i][i] = 1
 
-# 길이 2인 팰린드롬
-for i in range(N-1):
-    if arr[i] == arr[i+1]:
+for i in range(0,len(arr)-1):
+    if arr[i:i+2] == arr[i:i+2][::-1]:
         dp[i][i+1] = 1
 
-# 길이 3 이상인 팰린드롬
-for length in range(3, N+1):
-    for i in range(N - length + 1):
-        j = i + length - 1
-        if arr[i] == arr[j] and dp[i+1][j-1]:
-            dp[i][j] = 1
+# 1,1 -> 0,2
+# 2,2 -> 1,3 -> 0,4
+# . . .
+# 6,6 -> 5,7
 
-M = int(input())
-for _ in range(M):
-    S, E = map(int, input().split())
-    print(dp[S-1][E-1])
+for i in range(1,N):
+    for j in range(0,i+1):
+        x,y = i-j, i+j
+        if  0<=x-1<N and 0<=y+1<N:
+            if dp[x][y] and arr[x-1] == arr[y+1]:
+                dp[x-1][y+1] = 1
+
+
+# 1,2 -> 0,3
+# 2,3 -> 1,4 -> 0,5
+# . . .
+# 6,7 -> X
+
+
+for i in range(1, N):
+    for j in range(0, i + 1):
+        x, y = i - j, i + j + 1
+        if  0<=x-1<N and 0<=y+1<N:
+            if dp[x][y] and arr[x-1] == arr[y+1]:
+                dp[x-1][y+1] = 1
+
+
+
+T = int(input())
+for _ in range(T):
+    q,p = map(int,input().split())
+    print(dp[q-1][p-1])
+
+
+
+
