a48580561a20ae1a565885c9319e50952f8ce864
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-11 10:34:20 +0900

    [Silver III] Title: 달팽이, Time: 4136 ms, Memory: 125768 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,69 +1,48 @@
-import javax.swing.text.html.parser.Parser;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-
-    public static ArrayList<Integer> result = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
 
         int N = Integer.parseInt(br.readLine());
-        int cnt = N * N;
         int target = Integer.parseInt(br.readLine());
 
         int[] di = {1, 0, -1, 0};
         int[] dj = {0, 1, 0, -1};
 
+        int i = 0, j = 0, k = 0;
+        int cnt = N * N;
+
+
         int[][] graph = new int[N][N];
         graph[0][0] = cnt;
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
 
-        while(true){
+        while(cnt>0){
             int ni = i + di[k];
             int nj = j + dj[k];
 
-            if (0 <= ni && ni < N && 0 <= nj && nj < N) {
-                if(graph[ni][nj] == 0){
+            if (0 <= ni && ni < N && 0 <= nj && nj < N && graph[ni][nj] == 0) {
+                graph[ni][nj] = --cnt;
+                i = ni; j = nj;
+                if(cnt == 1){
                     cnt--;
-                    graph[ni][nj] = cnt;
-
-                    i = ni;
-                    j = nj;
-                }else if(cnt == 1){
-                    cnt--;
-
-                }else{
-                    k++;
                 }
             }else{
-                k++;
-            }
-
-            k %= 4;
-
-
-            if(cnt==0){
-                break;
+                k = (k + 1) % 4;
             }
         }
 
 
-
-        int x = 0;
-        int y = 0;
+        int x = 0, y = 0;
         for (int l = 0; l < N; l++) {
             for (int m = 0; m < N; m++) {
                 System.out.print(graph[l][m] + " ");
                 if(graph[l][m] == target){
-                    x = l+1;
-                    y = m+1;
+                    x = l+1; y = m+1;
                 }
             }
             System.out.println();
