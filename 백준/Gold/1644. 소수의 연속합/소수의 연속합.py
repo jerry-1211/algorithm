651af8682cacc8be7cac0f722e902e2270c29d7d
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-16 15:28:25 +0900

    [Gold III] Title: 소수의 연속합, Time: 3524 ms, Memory: 75144 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,43 @@
+N = int(input())
+
+MAX_VALUE = N + 1_000
+arr = [0] * MAX_VALUE
+arr[0],arr[1] = 1,1
+
+cnt = 2
+while cnt < MAX_VALUE:
+    value = cnt * 2
+    if arr[cnt] == 0:
+        for i in range(value,MAX_VALUE,cnt):
+            if i < MAX_VALUE:
+                arr[i] = 1
+            else:
+                break
+    cnt += 1
+
+
+lst = []
+for i in range(len(arr)):
+    if arr[i] == 0 and i <= N:
+        lst.append(i)
+
+
+start = 0
+end = 0
+
+cnt = 0
+while(True):
+
+    if sum(lst[start:end]) == N:
+        cnt += 1
+
+    if sum(lst[start:end]) <= N  and end < len(lst):
+        end += 1
+
+    elif sum(lst[start:end]) > N :
+        start += 1
+    else:
+        break
+
+
+print(cnt)
