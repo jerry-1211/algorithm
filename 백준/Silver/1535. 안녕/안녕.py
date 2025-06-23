9b62f477b33b43f7f157b6ea4ea9db405f30f19e
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-06-23 16:32:37 +0900

    [Silver II] Title: 안녕, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,13 +1,14 @@
-n = int(input())
-lose = list(map(int, input().split()))
-joy = list(map(int, input().split()))
+N = int(input())
+L = list(map(int, input().split()))  # 체력 감소량
+J = list(map(int, input().split()))  # 기쁨 증가량
 
-# dp[i] = 체력이 i일 때 얻을 수 있는 최대 기쁨
-dp = [0] * 100
+dp = [0] * 100  # 체력이 0~99일 때의 최대 기쁨
 
-for i in range(n):
-    # 뒤에서부터 계산해야 중복 계산 방지
-    for j in range(99, lose[i] - 1, -1):
-        dp[j] = max(dp[j], dp[j - lose[i]] + joy[i])
+for i in range(N):
+    loss = L[i]
+    joy = J[i]
+    # 뒤에서부터 갱신해야 중복 선택 방지됨 (0/1 Knapsack 패턴)
+    for hp in range(99, loss - 1, -1):
+        dp[hp] = max(dp[hp], dp[hp - loss] + joy)
 
 print(dp[99])
