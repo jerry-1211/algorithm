467de4ad47b3aaa81c0218124e04e41724684986
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-07-23 10:30:35 +0900

    [Silver II] Title: 사탕 게임, Time: 400 ms, Memory: 119140 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     static int[][] direction = {{0, 1}, {1, 0}};
@@ -24,12 +23,8 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < 2; k++) {
-
-
-
                     int ni = i + direction[k][0];
                     int nj = j + direction[k][1];
-
                     if (ni < N && nj < N) {
                         String[][] tempBomb = new String[N][N];
                         deepCopy(tempBomb);
@@ -39,19 +34,16 @@ public class Main {
 
                         result = Math.max(result,checkMaxCandy(tempBomb));
                     }
-
-
                 }
             }
         }
-
+        
         System.out.println(result);
+        
     }
 
     public static int checkMaxCandy(String[][] tempBomb) {
-
         int maxCount = 0;
-
         for (int i = 0; i < N; i++) {
             int count = 1;
             for (int j = 1; j < N; j++) {
@@ -64,8 +56,7 @@ public class Main {
             }
             maxCount = Math.max(maxCount, count);
         }
-
-
+        
         for (int j = 0; j < N; j++) {
             int count = 1;
             for (int i = 1; i < N; i++) {
@@ -81,9 +72,6 @@ public class Main {
 
         return maxCount;
     }
-
-
-
 
     public static void deepCopy(String[][] tempBomb){
         for (int i = 0; i < N; i++) {
