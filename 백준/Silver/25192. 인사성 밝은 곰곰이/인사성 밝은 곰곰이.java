d7231a6441bd806b2dcd0c6c510c7afe14d1d18f
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-08-27 10:03:14 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 240 ms, Memory: 27744 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashSet<String> set = new HashSet<>();
+
+        int N = Integer.parseInt(br.readLine());
+        int result = 0 ;
+        for(int i = 0; i < N; i++){
+            String name = br.readLine();
+            if(!"ENTER".equals(name)){
+                set.add(name);    
+            }else{
+                result += set.size();
+                set = new HashSet<>();
+            }
+        }
+
+        result += set.size();
+        System.out.println(result);
+        
+    }
+}
